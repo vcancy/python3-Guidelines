69cd2090190af9874b61ce836bdd6c8dce28f6c7
vcancy <yujie.teddy@outlook.com>
2018-04-02 22:22:41 +0800

    使用 pathlib 更好的对路径进行处理

---
+++ os-pathlib.py
@@ -0,0 +1,25 @@
+"""
+python3.4引入pathlib可以很方便的替换之前经常使用的os.path.join
+代码更安全，简洁，易读
+# doc: https://docs.python.org/3/library/pathlib.html
+# readme: https://pymotw.com/3/pathlib/
+p.exists()
+p.is_dir()
+p.parts()
+p.chmod()
+p.rmdir()
+"""
+
+import os
+from pathlib import Path
+
+dir = os.getcwd()
+
+path = Path(dir)
+
+test_path = path / 'test'
+
+
+for image_path in path.iterdir():
+	with image_path.open(encoding='utf-8') as f:
+		print(f.readlines())
